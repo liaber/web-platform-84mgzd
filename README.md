ca017d673a77d375889e3b828d8d5f117155b500
liaber <liamberube50@gmail.com>
2021-03-16 13:08:05 -0400

    1 file modified, 1 added, 3 deleted

--- README.md
+++
@@ -1,3 +0,0 @@
-# web-platform-84mgzd
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-84mgzd)
---
+++ script.js
@@ -0,0 +1,8 @@
+function add() {
+  let addend1 = document.getElementById("addend1");
+  console.log(addend1);
+  let addend2 = document.getElementById("addend2");
+  console.log(addend2);
+  var sum = addend1 + addend2;
+  document.getElementById("sum").valu = 'sum';
+};
